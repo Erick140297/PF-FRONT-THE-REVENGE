4b41370c68a698a50e4975a5fce62495ebb1fe08
Paula Ramírez <104044317+PaulaAn1@users.noreply.github.com>
2023-01-18 18:15:14 -0500

    componenteHome

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { GetAllProducts } from "../../Redux/Actions";
 import { useDispatch, useSelector } from "react-redux";
+import Card from "../Card/Card";
 
 
 
 const Home = () => {
   const Dispatch = useDispatch()
   const allComponents = useSelector((state)=>state.allProducts)
-    
+
   useEffect(()=>{
     Dispatch(GetAllProducts())
   }, []);
 
   return (
-    <div>Home</div>
+    <div>{allComponents?.map((el, index) => {
+      return (
+        <div key={el._id}>
+          <Card
+            _id={el._id}
+            name={el.name}
+            brand={el.brand}
+            category={el.category}
+            image={el.image ? el.image.secure_url : ""}
+            price={el.price}
+            stock={el.stock}
+            key={index}
+          />
+        </div>
+      );
+    })}
+    </div> 
   )
 }
 
-export default Home
+export default Home;
 
 
 
--- src/Redux/Actions.jsx
+++ src/Redux/Actions.jsx
@@ -6,7 +6,6 @@ export function GetAllProducts(){
     try {
         return async function(dispatch){
             const response = await axios.get("http://localhost:3001/products")      
-            console.log(response);
             dispatch({
                 type: 'GET_ALL_PRODUCTS',
                 payload: response.data
