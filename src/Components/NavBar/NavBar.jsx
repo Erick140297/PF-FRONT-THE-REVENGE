8d9b95b4636205aa4c3dbdac86715b95f163adfc
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-19 17:00:14 -0600

    navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import SearchBar from "./SearchBar/SearchBar";
 
 const NavBar = () => {
   return (
     <>
       <NavContainer>
-        <h2>Galaxia Tech</h2>
-        <form action="">
-          <input type="text" />
-          <input type="submit" />
-        </form>
-        <div>
-          <button>Login</button>
-          <span>🛒{0}</span>
-        </div>
+        <ContainerLogo >
+          <img
+            src="https://res.cloudinary.com/dfaxzahb0/image/upload/v1674156994/Products/GT_onvscx.png"
+            alt="Logo"
+          />
+          <h2>Galaxia Tech</h2>
+        </ContainerLogo>
+        <SearchBar/>
+          <span>Mi cuenta</span>
+          <span>🛒 Mi carrito {0}</span>
       </NavContainer>
     </>
   );
@@ -23,10 +25,10 @@ export default NavBar;
 
 const NavContainer = styled.nav`
   h2 {
-    color: white;
-    font-weight: 400;
+    color: #7230FF;
+    font-weight: 600;
   }
-  span{
+  span {
     color: white;
   }
   width: 100%;
@@ -37,3 +39,17 @@ const NavContainer = styled.nav`
   justify-content: space-around;
   background-color: black;
 `;
+
+const ContainerLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  h2{
+    margin-left: 10px;
+  }
+`
+
+const Count = styled.div`
+  
+`
+
+const Cart = styled.div``
