45b6262fa5dd713297aedb2a4dfca2470bd47171
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-20 09:46:17 -0600

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,49 @@
-import './App.css'
-import { Route } from 'react-router-dom'
-import Home from './Components/Home/Home.jsx'
-import NavBar from './Components/NavBar/NavBar'
-import Footer from './Components/Footer/Footer'
-import Header from './Components/SideBar/Header';
-import Result from './Components/Result/Result';
-import Profile from './Components/Profile/Profile'
-import React, {useState, useEffect} from "react"
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./Components/Home/Home.jsx";
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/SideBar/Header";
+import Result from "./Components/Result/Result";
+import Profile from "./Components/Profile/Profile";
+import React, { useState, useEffect } from "react";
 import RingLoader from "react-spinners/RingLoader";
 
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setLoading(true)
-    setTimeout(()=>{
-      setLoading(false)
-    }, 3000)
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
   }, []);
 
   return (
     <div className="App">
-
-
-{
-      loading ?
-      <div className='loading'>
-      <RingLoader
-      color={"#5f36d6"}
-      loading={loading}
-      size={150}
-      aria-label="Loading Spinner"
-      data-testid="loader"
-    />
+      {loading ? (
+        <div className="loading">
+          <RingLoader
+            color={"#5f36d6"}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <div className="otracosa">
+            <Route exact path="/" component={Header} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/result" component={Result} />
+            <Route exact path="/profile" component={Profile} />
+            <Footer />
+          </div>
+        </div>
+      )}
     </div>
-
-      :
-
-
-
-
-    <div>
-      <NavBar/>
-      <div className="otracosa">
-      <Route exact path="/" component={Header} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/result" component={Result} />
-      <Route exact path="/profile" component={Profile} />
-      <Footer/>
-      </div>
-    </div>
-
+  );
 }
 
-    </div>
-  )
-}
-
-export default App
+export default App;
