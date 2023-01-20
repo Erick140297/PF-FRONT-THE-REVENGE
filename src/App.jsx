940fae1f29f2c1f56c4b22277c1feb5670b4c05b
JuanCruz <juancruzduarte1997@gmail.com>
2023-01-19 21:14:40 -0300

    carrusel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Home from './Components/Home/Home.jsx'
 import NavBar from './Components/NavBar/NavBar'
-
+import Footer from './Components/Footer/Footer'
 function App() {
 
   return (
     <div className="App">
       <NavBar/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
