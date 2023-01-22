f25fc58c9e8f7a92728fc361de230c6705759dfe
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-22 13:07:48 -0600

    Profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { logout } = useAuth0();
+  const { logout, user  } = useAuth0();
+  console.log(user)
   return (
     <div>
+      <p>Hola {user.name}</p>
       <button
         onClick={() =>
           logout({ logoutParams: { returnTo: window.location.origin } })
