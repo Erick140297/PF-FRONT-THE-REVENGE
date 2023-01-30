1e9d0384edad070ebdce63a7a3f595960f1ea7f0
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-30 09:33:24 -0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import './footer.css'
+import Button from "react-bootstrap/Button";
+import React from "react";
+import "./footer.css";
+import whatsapp from "../../assets/whatsapp.svg";
+import gmail from "../../assets/gmail.svg";
+import intel from "../../assets/intel.svg";
+import amd from "../../assets/amd.svg";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -20,7 +25,7 @@ const Footer = () => {
           <div className="col-md-1 mb-md-0 mb-3 marcas">
             <h5 className="text-uppercase">Marcas</h5>
             <ul className="list-unstyled">
-              {/* <li>
+              <li>
                 <Button variant="outline-danger">
                   <img src={amd} alt="amd" />
                 </Button>
@@ -29,29 +34,54 @@ const Footer = () => {
                 <Button variant="outline-info">
                   <img src={intel} alt="intel" />
                 </Button>
-              </li> */}
+              </li>
             </ul>
           </div>
-           <div className="col-md-1 mb-md-0 mb-3">
+          {/* <div className="col-md-1 mb-md-0 mb-3">
                 <h5 className="text-uppercase">Ayuda</h5>
                 <ul className="list-unstyled">
                     <li >WhatsApp</li>
                     <li>Mail</li>
                 </ul>
-            </div>
-            <div className="col-md-1 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Integra</h5>
-                <ul className="list-unstyled">
-                <Link to="/about">
-                <button className="buttonFooter">Equipo</button>
-                </Link>
-                    <li><a href="#!">Link 2</a></li>
-                </ul>
-            </div>
-            <div className="col-md-5 mt-md-0 mt-4">
-                <h5 className="text-uppercase">Galaxia Tech</h5>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure minus corporis, necessitatibus natus et consectetur provident ratione obcaecati expedita adipisci. Esse aut eveniet necessitatibus vitae illo corporis vero rerum veniam.</p>
-            </div>
+            </div> 
+            
+            https://wa.link/bdmxpb
+            */}
+          <div className="col-md-1 mb-md-0 mb-3 contacto">
+            <h5 className="text-uppercase">Contacto</h5>
+            <ul className="list-unstyled">
+              <a
+                href="https://wa.link/glhjh5"
+                target="_blank"
+                rel="noopener noregerrer"
+              >
+                <li>
+                  <Button variant="outline-success">
+                    Whatsapp⠀
+                    <img src={whatsapp} alt="lo" />
+                  </Button>
+                </li>
+              </a>
+              <a href="mailto:santicasas667@gmail.com">
+                <li>
+                  <Button variant="outline-danger">
+                    ⠀⠀Mail⠀⠀⠀
+                    <img src={gmail} alt="gmail" />⠀
+                  </Button>
+                </li>
+              </a>
+            </ul>
+          </div>
+          <div className="col-md-5 mt-md-0 mt-4 leyenda">
+            <h5 className="text-uppercase">Galaxia Tech</h5>
+            <p>
+              Nos dedicamos a la venta de los mejores componentes de PC, te
+              ofrecemos los componentes esenciales para tu PC, junto con una
+              gran gama de perrifericos para asegurar una experiencia comoda y
+              los respectivos equipos de mantenimiento para que puedas cuidar de
+              tu preciada reliquia
+            </p>
+          </div>
         </div>
       </div>
     </footer>
