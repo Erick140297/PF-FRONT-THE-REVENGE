9bf5667ccfe61d7e4b8d82003d99049e45b8a130
HydraRosario <acevedo_marcos17@hotmail.com>
2023-02-03 05:03:28 -0300

    reviews

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -51,7 +51,7 @@ const Detail = (props) => {
   useEffect(() => {
     dispatch(GetAllProducts())
     dispatch(getDetail(id));
-    
+    console.log(details.rating)
     return () => {
       dispatch(setLoader());
     };
@@ -121,10 +121,10 @@ const Detail = (props) => {
                     <div>
                       <h4 className="h-rating">Rating del producto</h4>
                       <Rating
-                        name="half-rating-read"
+                        name="raintg"
                         defaultValue={
-                          details?.rating ? (
-                            promedio(details?.rating)
+                          details?.rating.length ? (
+                            details.rating.length
                           ) : (
                             <h3>Sin rating</h3>
                           )
@@ -135,6 +135,24 @@ const Detail = (props) => {
                     </div>
                   ) : (
                     <h3>Sin rating aún</h3>
+                  )}
+                </div>
+                <br />
+                <div className="review">
+                  {details.review ? (
+                    <div>
+                      <h2>Reviews:</h2>
+                      <Review>
+                      <h3 className="user">
+                        Usuario: {details.review.map(e=>e.user)}
+                      </h3>
+                      <h3 className="comentario">
+                        Comentario: {details.review.map(e=>e.comentario)}
+                      </h3>
+                    </Review>
+                    </div>
+                  ) : (
+                    <h3>Sin review aún</h3>
                   )}
                 </div>
                 <div className="product-description-container">
@@ -179,4 +197,12 @@ const Relation = styled.div`
   width: 100%;
   margin: 20px;
   padding: 20px;
+`
+
+const Review = styled.div`
+  justify-content: center;
+  background-color: #d7dce1;
+  width: 100%;
+  padding: 20px;
+  border-radius: 10px;
 `
