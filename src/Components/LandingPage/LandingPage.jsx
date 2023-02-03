8d1939a1368cf1550f53fd8dee030c8f39e42263
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-02 20:42:07 -0600

    home

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import Loader from "../Loader/Loader";
 import styles from "./LandingPage.css";
 import video1 from "../../assets/video1.mp4";
 import video2 from "../../assets/video2.mp4";
@@ -17,44 +18,51 @@ export default function LandingPage() {
 
   useEffect(() => {
     if (isAuthenticated) {
+        
       redirect();
     }
   }, [isAuthenticated]);
 
   return (
-    <div className="landing">
-      <div className="imgWrapper4">
-        <div className="imgResponsiva">
-          <video src={video1} autoPlay loop muted></video>
+    <>
+      {isAuthenticated ? (
+        <Loader />
+      ) : (
+        <div className="landing">
+          <div className="imgWrapper4">
+            <div className="imgResponsiva">
+              <video src={video1} autoPlay loop muted></video>
+            </div>
+            <div className="imgResponsiva">
+              <video src={video2} autoPlay loop muted></video>
+            </div>
+            <div className="imgResponsiva">
+              <video src={video3} autoPlay loop muted></video>
+            </div>
+            <div className="imgResponsiva">
+              <video src={video5} autoPlay loop muted></video>
+            </div>
+          </div>
+          <div className="header-overlay"></div>
+          <div className="header-content">
+            <h1 className="h4 text-center pb-2 mb-4 border-bottom border-light">
+              BieNvEnId@ A GaLaXIA Tech!
+            </h1>
+            <p className="text-center fw-bold">
+              <i></i> Un luNgAr dOndE enConTrArás lOs MejORes COMPONENTES pARa
+              aRMar o aCtUaLizar Tu PC.
+            </p>
+            <Link to="/home" style={{ textDecoration: "none" }}>
+              <button className="btn btn-dark">
+                <i>
+                  <FaRocket />
+                </i>{" "}
+                INGRESAR
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="imgResponsiva">
-          <video src={video2} autoPlay loop muted></video>
-        </div>
-        <div className="imgResponsiva">
-          <video src={video3} autoPlay loop muted></video>
-        </div>
-        <div className="imgResponsiva">
-          <video src={video5} autoPlay loop muted></video>
-        </div>
-      </div>
-      <div className="header-overlay"></div>
-      <div className="header-content">
-        <h1 className="h4 text-center pb-2 mb-4 border-bottom border-light">
-          BieNvEnId@ A GaLaXIA Tech!
-        </h1>
-        <p className="text-center fw-bold">
-          <i></i> Un luNgAr dOndE enConTrArás lOs MejORes COMPONENTES pARa aRMar
-          o aCtUaLizar Tu PC.
-        </p>
-        <Link to="/home" style={{ textDecoration: "none" }}>
-          <button className="btn btn-dark">
-            <i>
-              <FaRocket />
-            </i>{" "}
-            INGRESAR
-          </button>
-        </Link>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
